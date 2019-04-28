3402db9cc9b58610d65c715b109f44d9f44da050
mukhsia <mukhsia@pdx.edu>
2019-04-27 22:57:01 -0700

    Update 3-http.js
    More additions, redirect, cache, cookie, check; Still need to do local checking.

--- node/3-http.js
+++ node/3-http.js
@@ -6,39 +6,54 @@ const server = http.createServer(function(req,res) {
 
 
 // http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
-if (req.url.indexOf('/missing') === 0) {
-    res.writeHead(404, {
-        'Content-Type': 'text/plain'
-    });
+    if (req.url.indexOf('/missing') === 0) {
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
 
-    res.write('your princess is in another castle');
-}
+        res.write('your princess is in another castle');
+    }
 // http://localhost:8080/redirect should redirect the request to '/redirected' by using 302 as the status code
+    if (req.url.indexOf('/redirect') === 0) {
+        res.writeHead(302, {
+            'Location': 'http://localhost:8080/redirected'
+        });
+    }
 
 // http://localhost:8080/cache should return 'cache this resource' in plain text and set the cache max age to a day
+if (req.url.indexOf('/cache') === 0) {
+    res.writeHead(302, {
+        'Content-Type': 'text/plain',
+        'Cache-Control': 'max-age=86400000'
+    });
+    res.write('cache this resource');
+}
 
 // http://localhost:8080/cookie should return 'i gave you a cookie' in plain text and set 'hello=world' as a cookie
-else if (req.url.indexOf('/cookie') === 0) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Set-Cookie': 'hello=world'
-    });
+    else if (req.url.indexOf('/cookie') === 0) {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+            'Set-Cookie': 'hello=world'
+        });
 
-    res.write('<!DOCTYPE html><html><body>set cookie exercise, expects \'hello = world\' cookie</body></html>');
-}
+        res.write('i gave you a cookie');
+    }
 
 // http://localhost:8080/check should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
-else if (req.url.indexOf('/check') === 0) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    if(req.headers.cookie != null && req.headers.cookie['hello'] != null)
-    {
-        res.write('yes');
+    else if (req.url.indexOf('/check') === 0) {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        if(req.headers.cookie != null && req.headers.cookie['hello'] != null)
+        {
+            res.write('yes');
+        }
+        res.write('no');
     }
-    res.write('no');
-}
+    res.end();
 });
+
+
 
 console.log('server listening on port 8080');
 server.listen(process.env.PORT || 8080);
