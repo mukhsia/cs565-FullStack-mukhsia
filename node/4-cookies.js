399152bc89cc4398d14015f1d6630a8f87122f0b
mukhsia <mukhsia@pdx.edu>
2019-04-30 16:51:08 -0700

    Initial attempt, added favicon.ico exception just in case: attempt 1

--- node/4-cookies.js
+++ node/4-cookies.js
@@ -23,3 +23,52 @@ var http = require('http'); // do not change this line
 // [the server restarts and looses all cookies]
 
 // http://localhost:8080/servus should return 'you must be new' in plain text and set an ident cookie
+
+var url = require("url");
+
+var cookieIdentCount = 0;
+var cookieIdents = {};
+var lastUrl = {};
+
+const server = http.createServer(function(req,res) {
+    if (req.url.indexOf('/') === 0 && req.url !== '/favicon.ico') {
+        var packedCookie = req.headers.cookie;
+        var cookies = {};
+        packedCookie && packedCookie.split(';').forEach(function(cookie) {
+            var parts = cookie.split('=');
+            cookies[parts.shift().trim()] = decodeURI(parts.join('='));
+        });
+
+        if(cookies['ident'] == null || Number(cookies['ident']) > cookieIdentCount)
+        {
+            cookieIdentCount = cookieIdentCount + 1;
+            var cookieval = 'ident=' + cookieIdentCount;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain',
+                'Set-Cookie': cookieval
+            });
+            res.write('you must be new');
+            console.log(cookies['ident']);
+            console.log(lastUrl[cookies['ident']]);
+            lastUrl[cookies['ident']] = url.parse(req.url).pathname;
+            console.log(lastUrl[cookies['ident']]);
+            res.end();
+        }
+        else
+        {
+            res.writeHead(200, {
+                'Content-Type': 'text/plain',
+            });
+            console.log(cookies['ident']);
+            console.log(lastUrl[cookies['ident']]);
+            res.write('last time you visited ' + lastUrl[cookies['ident']]);
+            lastUrl[cookies['ident']] = url.parse(req.url).pathname;
+            res.end();
+        }
+    }
+});
+
+
+
+console.log('server listening on port 8080');
+server.listen(process.env.PORT || 8080);
