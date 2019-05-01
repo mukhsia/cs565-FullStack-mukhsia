96736c8ce731284502dabc86d8d119cf6c33879b
mukhsia <mukhsia@pdx.edu>
2019-04-30 18:23:04 -0700

    Initial form attempt works locally; attempt 1

--- node/5-form.js
+++ node/5-form.js
@@ -21,15 +21,62 @@ var querystring = require('querystring'); // do not change this line
 //   
 //   res.end();
 
-var server = http.createServer(function(req, res) {
-    if(req.method === 'GET')
+var messages = "";
+
+const server = http.createServer(function(req, res) {
+    if(req.url === '/form')
     {
+       res.writeHead(200, {
+       	'Content-Type': 'text/html'
+       });
+       
+       res.write('<!DOCTYPE html>');
+       res.write('<html>');
+       	res.write('<body>');
+       		res.write('<form action="/new" method="post">');
+       			res.write('<input type="text" name="name">');
+       			res.write('<input type="text" name="message">');
+       			res.write('<input type="submit" value="submit">');
+       		res.write('</form>');
+       	res.write('</body>');
+       res.write('</html>');
+    }
+
+    else if(req.url === '/new' && req.method === 'POST')
+    {
+        var body = '';
+
+        req.on('data', function(data) {
+            body += data;
+        });
+
+        req.on('end', function () {
+            var post = querystring.parse(body);
+            if(messages.length === 0)
+            {
+                messages = messages + post['name'] + ": " + post['message'];
+            }
+            else
+            {
+                messages = messages + "\n" + post['name'] + ": " + post['message'];
+            }
+        });
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+        });
+        res.write('thank you for your message');
+    }
+
+    else if (req.url === '/list')
+    {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+        });
+        res.write(messages);
 
     }
-    else if(req.method === 'POST')
-    {
-
-    }
+    res.end();
 
 // http://localhost:8080/new should retrieve the post data, save the name / message (in a global variable) and return 'thank you for your message' in plain text
 
