67056160be4dd80ccdf948699b3ca8eb6cd5a03f
mukhsia <mukhsia@pdx.edu>
2019-05-12 12:41:05 -0700

    Initial form using bodyparser and req.body object; Attempt 1

--- express/5-form.js
+++ express/5-form.js
@@ -24,17 +24,70 @@ var parser = require('body-parser'); // do not change this line
 //   res.write('</html>');
 //   
 //   res.end();
-
-// http://localhost:8080/new should retrieve the post data, save the name / message (in a global variable) and return 'thank you for your message' in plain text
-
-// http://localhost:8080/list should return the stored messages (from the global variable) 'name: message' in plain text
+const server = express();
+var messages = "";
+server.use(function(req, res, next) {
+    next();
+});
+server.use(parser.json());
+server.use(parser.urlencoded());
 
 // http://localhost:8080/form should return the form as shown above
+server.get('/form', function(req, res) {
+    res.status(200);
+    
+    res.set({
+    	'Content-Type': 'text/html'
+    });
+
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+     	res.write('<body>');
+    	    res.write('<form action="/new" method="post">');
+    			res.write('<input type="text" name="name">');
+     			res.write('<input type="text" name="message">');
+    		res.write('<input type="submit" value="submit">');
+        	res.write('</form>');
+        res.write('</body>');
+    res.write('</html>');
+
+    res.end();
+})
+
 
 // http://localhost:8080/new should retrieve the post data using the body parser, save the name / message (in a global variable) and return 'thank you for your message' in plain text
+server.post('/new', function(req, res) {
+    var name = req.body.name;
+    var message = req.body.message;
+    if(messages.length === 0)
+    {
+        messages = messages + name + ": " + message;
+    }
+    else
+    {
+        messages = messages + "\n" + name + ": " + message;
+    }
+    res.status(200);
+    res.set({
+        'Content-Type': 'text/plain',
+    });
+    res.write("thank you for the message");
+    res.end();
+});
 
 // http://localhost:8080/list should return the stored messages (from the global variable) 'name: message\nanother name: another message' in plain text
+server.get('/list', function(req, res) {
 
+    res.status(200);
+    res.set({
+        'Content-Type': 'text/plain',
+    });
+    res.write(messages);
+    res.end();
+});
 // [the server restarts and looses all messages]
 
 // http://localhost:8080/list should return '' in plain text
+
+
+server.listen(process.env.PORT || 8080);
