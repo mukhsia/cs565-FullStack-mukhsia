ed3bf2e4e335735ac6baf11d87871747aba52423
mukhsia <mukhsia@pdx.edu>
2019-05-12 11:41:24 -0700

    Initial sessions, works fine and as expected locally; Attempt 1

--- express/4-sessions.js
+++ express/4-sessions.js
@@ -26,3 +26,41 @@ var session = require('express-session'); // do not change this line
 // [the server restarts and looses all cookies]
 
 // http://localhost:8080/servus should return 'you must be new' in plain text and implicitly set an ident cookie by using the session middleware
+const server = express();
+var storage = {};
+server.use(function(req, res, next) {
+    next();
+});
+
+server.use(session({secret: 'secret'}));
+
+server.get('/favicon.ico' , function(req, res) {
+    res.end();
+})
+
+server.get('/*', function(req, res) {
+    res.status(200);
+    res.set({
+        'Content-Type': 'text/plain',
+    });
+    
+    var paths = storage[req.sessionID];
+    if(paths === undefined)
+    {
+        res.write('you must be new');
+
+        storage[req.sessionID] = new Array(req.path);
+    }
+    else
+    {
+        res.write("your history:")
+        for(let path in paths)
+        {
+            res.write("\n " + paths[path]);
+        }
+        paths.push(req.path);
+    }
+    res.end();
+})
+
+server.listen(process.env.PORT || 8080);
